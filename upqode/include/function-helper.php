53ac1480050fe582cac56e5919b49c6a3a4249d8
Orest Matsukh <orest.sd@gmail.com>
2023-01-27 23:28:44 +0200

    add

--- upqode/include/function-helper.php
+++ upqode/include/function-helper.php
@@ -4,6 +4,62 @@ require_once ABSPATH . 'wp-admin/includes/plugin.php';
 
 add_action('upqode_search', 'upqode_search_popup', 10);
 add_filter('the_password_form', 'upqode_password_form');
+add_action('after_setup_theme', 'upqode_setup');
+
+
+if (!function_exists('upqode_setup')) :
+
+	function upqode_setup()
+	{
+		add_theme_support('html5', array('search-form', 'comment-form', 'comment-list', 'gallery', 'caption'));
+		add_theme_support('post-formats', array(
+			'aside', 'image', 'video', 'quote', 'link', 'gallery', 'status', 'audio', 'chat'
+		));
+
+
+		load_theme_textdomain('upqode', get_template_directory() . '/languages');
+
+		// Add default posts and comments RSS feed links to head.
+		add_theme_support('automatic-feed-links');
+
+		/*
+		*  Let WordPress manage the document title.
+		* By adding theme support, we declare that this theme does not use a
+		* hard-coded <title> tag in the document head, and expect WordPress to
+		* provide it for us.
+		*/
+		add_theme_support('title-tag');
+
+		// Add theme support for selective refresh for widgets.
+		add_theme_support('customize-selective-refresh-widgets');
+
+		// Add support for Block Styles.
+		add_theme_support('wp-block-styles');
+
+		// Add support for full and wide align images.
+		add_theme_support('align-wide');
+
+		// Add support for editor styles.
+		add_theme_support('editor-styles');
+
+		// Add support for responsive embeds.
+		add_theme_support('responsive-embeds');
+
+		// Enqueue editor styles.
+		add_editor_style(array('assets/css/style.min.css'));
+
+		/*
+	 * Enable support for Post Thumbnails on posts and pages.
+	 *
+	 * See: https://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
+	 */
+		add_theme_support('post-thumbnails');
+		set_post_thumbnail_size(1200, 750, true);
+	}
+endif;
+
+
+
 
 /**
  * Create custom html structure for comments
--- upqode/include/optimization-html.php
+++ upqode/include/optimization-html.php
@@ -1,7 +1,8 @@
 <?php
 
-
+// Disable REST API link tag
 remove_action('wp_head', 'rest_output_link_wp_head', 10);
+
 remove_action('wp_enqueue_scripts', 'wp_enqueue_global_styles');
 remove_action('wp_footer', 'wp_enqueue_global_styles', 1);
 remove_action('wp_body_open', 'wp_global_styles_render_svg_filters');
