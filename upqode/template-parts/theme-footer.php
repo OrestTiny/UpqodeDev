f62d6fef52fd496fa76093268d3604847c336c24
orest-upqode <orest.h.upqode@gmail.com>
2023-01-29 21:13:53 +0200

    header

--- upqode/template-parts/theme-footer.php
+++ upqode/template-parts/theme-footer.php
@@ -6,7 +6,17 @@ $footer_text = esc_html__(' &copy;', 'upqode') . date('Y') . ' ' . get_bloginfo(
 
 <footer class="upqode-footer">
     <div class="container">
-        <div class="upqode-footer--copyright"><?php echo wp_kses($footer_text, 'upqode'); ?></div>
+        <div class="upqode-footer__menu">
+            <?php if (has_nav_menu('footer-menu')) {
+                $args = array(
+                'container' => false,
+                'menu_class' => 'footer-menu',
+                );
+                $args['theme_location'] = 'footer-menu';
+                wp_nav_menu($args);
+            } ?>
+        </div>
+        <div class="upqode-footer__copyright"><?php echo wp_kses($footer_text, 'upqode'); ?></div>
     </div>
 </footer>
 
