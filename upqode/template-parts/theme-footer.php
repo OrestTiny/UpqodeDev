d02c40c4ddb76ac8042a443044ed7c3f3aed3bdd
Orest Matsukh <orest.sd@gmail.com>
2023-01-30 01:57:59 +0200

    add

--- upqode/template-parts/theme-footer.php
+++ upqode/template-parts/theme-footer.php
@@ -7,26 +7,24 @@ $footer_text = esc_html__(' &copy;', 'upqode') . date('Y') . ' ' . get_bloginfo(
 <footer class="upqode-footer">
     <div class="container">
         <div class="upqode-footer__wrap">
-            <div class="upqode-footer__copyright"><?php echo wp_kses($footer_text, 'upqode'); ?></div>
+            <div class="upqode-footer__menu">
+                <?php if (has_nav_menu('footer-menu')) {
+                    $args = array(
+                        'container' => false,
+                        'menu_class' => 'footer-menu',
+                    );
+                    $args['theme_location'] = 'footer-menu';
+                    wp_nav_menu($args);
+                } ?>
+            </div>
             <div class="upqode-footer__development">
                 <a href="https://upqode.com/web-design/" rel="noopener" target="_blank">Web Design</a> and
                 <a href="https://upqode.com/wordpress-development/" rel="noopener" target="_blank">Development</a> by
                 <a href="https://upqode.com/" rel="noopener" target="_blank">UPQODE</a>
             </div>
+            <div class="upqode-footer__copyright"><?php echo wp_kses($footer_text, 'upqode'); ?></div>
         </div>
-        <div class="upqode-footer__menu">
-            <?php if (has_nav_menu('footer-menu')) {
-                $args = array(
-                    'container' => false,
-                    'menu_class' => 'footer-menu',
-                );
-                $args['theme_location'] = 'footer-menu';
-                wp_nav_menu($args);
-            } ?>
-        </div>
-        <div class="upqode-footer__copyright"><?php echo wp_kses($footer_text, 'upqode'); ?></div>
     </div>
 </footer>
-
 
 </div>
